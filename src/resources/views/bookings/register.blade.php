6b30b0a98b54bd2000c1c53535ac57da4abca4f5
Kevin Alberca <kevinalbercacubas@gmail.com>
2024-11-12 14:07:24 -0500

    fix(bookings): actualizando texto

--- src/resources/views/bookings/register.blade.php
+++ src/resources/views/bookings/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Registrar reserva') }}</div>
 
                 <div class="card-body">
                     <form action="{{ route('bookings.store') }}" method="POST">
